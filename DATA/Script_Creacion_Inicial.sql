0bdb1e4b812b2097d2d5e4cb41273441f2a30215
GabiAlifraco <gabriel.alifraco@gmail.com>
2017-06-30 19:17:59 -0300

    Agrego restricciones que faltaban en las tablas, organizo y tuneo; ademas agrego el DER modificado

--- DATA/Script_Creacion_Inicial.sql
+++ DATA/Script_Creacion_Inicial.sql
@@ -1,28 +1,159 @@
 USE [GD1C2017]
 GO
 
-/* CREACION DEL ESQUEMA */
+/* ################################# CREACION DEL ESQUEMA ################################# */
 
 CREATE SCHEMA [HAY_TABLA] AUTHORIZATION [gd]
 GO
 
-/* CREACION DE TABLAS MAESTRAS (ABM) */
-BEGIN TRAN
+/* ############################## CREACION DE TABLAS MAESTRAS ############################## */
 
 CREATE TABLE [HAY_TABLA].[Usuarios](
-	Usu_Id int NOT NULL IDENTITY(1,1),
-	Usu_Username VARCHAR(30) NOT NULL,
-	Usu_Password VARBINARY(8000) NOT NULL,
-	Usu_IntentosFallidos TINYINT NOT NULL DEFAULT 0
-	CONSTRAINT PK_Usuarios PRIMARY KEY (Usu_Username)
+	Usu_Id 							INT NOT NULL IDENTITY(1,1),
+	Usu_Username 					VARCHAR(30) NOT NULL,
+	Usu_Password 					VARBINARY(8000) NOT NULL,
+	Usu_IntentosFallidos 			TINYINT NOT NULL DEFAULT 0,
+	CONSTRAINT PK_Usuarios 			PRIMARY KEY (Usu_Username)
 );
+
+CREATE TABLE [HAY_TABLA].Chofer (
+    Cho_Id 							INT NOT NULL IDENTITY(1,1),
+	Cho_Usuario 					VARCHAR(30), /*** FK **/
+    Cho_Nombre 						VARCHAR(30) NOT NULL,
+    Cho_Apellido 					VARCHAR(255),
+	Cho_DNI 						NUMERIC(18, 0),
+	Cho_Mail 						VARCHAR(50),
+	Cho_Telefono 					NUMERIC(18, 0),
+	Cho_Direccion 					VARCHAR(255),
+	Cho_FechaNacimiento 			DATETIME,
+	CONSTRAINT PK_Chofer 			PRIMARY KEY (Cho_ID),
+	FOREIGN KEY (Cho_Usuario) 		REFERENCES [HAY_TABLA].Usuarios(Usu_Username)
+);
+
+CREATE TABLE [HAY_TABLA].Cliente(
+	Cli_Id 							INT NOT NULL IDENTITY(1,1),
+	Cli_Usuario 					VARCHAR(30), /** FK **/
+	Cli_Nombre 						VARCHAR(255),
+	Cli_Apellido 					VARCHAR(255),
+	Cli_DNI 						NUMERIC(18, 0),
+	Cli_Mail 						VARCHAR(255),
+	Cli_Telefono 					NUMERIC(18, 0),
+	Cli_Direccion 					VARCHAR(255),
+	Cli_CodigoPostal				NUMERIC(10, 0),
+	Cli_FechaNacimiento 			DATETIME,
+	CONSTRAINT PK_Cliente 			PRIMARY KEY (Cli_Id),
+	FOREIGN KEY (Cli_Usuario) 		REFERENCES [HAY_TABLA].Usuarios(Usu_Username)
+);
+
+CREATE TABLE [HAY_TABLA].Automovil(
+	Auto_Id 						INT NOT NULL IDENTITY(1,1),
+	Auto_Patente 					VARCHAR(10),
+	Auto_Marca 						VARCHAR(20),
+	Auto_Modelo 					VARCHAR(255),
+	Auto_Licencia 					VARCHAR(26),
+	Auto_Rodado 					VARCHAR(10),
+	Auto_Habilitado				 	BIT DEFAULT '1',
+	CONSTRAINT PK_Automovil 		PRIMARY KEY (Auto_Id),
+	CONSTRAINT AK_AutoPatente 		UNIQUE(Auto_Patente)   
+);
+
+CREATE TABLE [HAY_TABLA].[Turno](
+	Turno_Id 						INT NOT NULL IDENTITY(1,1),
+	Turno_HoraInicio 				NUMERIC(18, 0),
+	Turno_HoraFin 					NUMERIC(18, 0),
+	Turno_Descripcion 				VARCHAR(255),
+	Turno_ValorKM 					NUMERIC(18, 2),
+	Turno_PrecioBase 				NUMERIC(18, 2),
+	Turno_Habilitado 				BIT DEFAULT '1',
+	CONSTRAINT PK_Turno 			PRIMARY KEY (Turno_Id)
+);
+
+
+CREATE TABLE [HAY_TABLA].AsignacionDeTurnos(
+	Asignacion_Id 					INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Turno_Id 						INT NOT NULL,
+	Cho_Id 							INT NOT NULL,
+	Auto_Id 						INT NOT NULL,
+	FOREIGN KEY (Turno_Id) 			REFERENCES [HAY_TABLA].Turno(Turno_Id),
+	FOREIGN KEY (Cho_Id)   			REFERENCES [HAY_TABLA].Chofer(Cho_Id),
+	FOREIGN KEY (Auto_Id)  			REFERENCES [HAY_TABLA].Automovil(Auto_Id)
+ );
+
+CREATE TABLE [HAY_TABLA].[ROL] (
+	[Id_Rol]						INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[Nombre]						VARCHAR(50) NOT NULL,
+	[Habilitado]					BIT DEFAULT 1
+);
+
+CREATE TABLE [HAY_TABLA].[USUARIO_POR_ROL] (
+	[Id_Rol]						INT NOT NULL REFERENCES [HAY_TABLA].[ROL](Id_Rol),
+	[Nombre_Usuario]				VARCHAR(30) NOT NULL REFERENCES [HAY_TABLA].[Usuarios](Usu_Username),
+	[Habilitado]					BIT DEFAULT 1,
+	PRIMARY KEY (Id_Rol, Nombre_Usuario),
+	FOREIGN KEY (Nombre_Usuario)  	REFERENCES [HAY_TABLA].Usuarios(Usu_Username),
+	FOREIGN KEY (Id_Rol)  			REFERENCES [HAY_TABLA].ROL(Id_Rol)
+);
+
+CREATE TABLE [HAY_TABLA].[FUNCIONALIDAD] (
+	[Id_Funcionalidad]				INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[Descripcion]					VARCHAR(255) NOT NULL	
+);
+
+CREATE TABLE [HAY_TABLA].[FUNCIONALIDAD_POR_ROL] (
+	[Id_Funcionalidad]				INT NOT NULL,
+	[Id_Rol]						INT NOT NULL,
+	FOREIGN KEY (Id_Funcionalidad)  REFERENCES [HAY_TABLA].FUNCIONALIDAD(Id_Funcionalidad),
+	FOREIGN KEY (Id_Rol)  			REFERENCES [HAY_TABLA].ROL(Id_Rol)
+);
+
+CREATE TABLE [HAY_TABLA].Viaje(
+	Id_Viaje 						INT NOT NULL IDENTITY(1,1),
+	Vi_IdChofer 					INT,
+	Vi_IdCliente 					INT,
+	Vi_AutoPatente 					VARCHAR(10),
+	Vi_IdTurno 						INT, 
+	Vi_CantKilometros 				NUMERIC(18, 0),
+	Vi_Inicio 						DATETIME,
+	Vi_Fin 							DATETIME,
+	Rendicion_Nro 					NUMERIC(18,0),
+	Factura_Nro 					NUMERIC(18,0),
+	Vi_ImporteTotal 				NUMERIC(18, 2),
+	CONSTRAINT PKViaje 				PRIMARY KEY (Id_Viaje),
+	FOREIGN KEY (Vi_IdChofer) 		REFERENCES [HAY_TABLA].Chofer(Cho_Id),
+	FOREIGN KEY (Vi_IdCliente)		REFERENCES [HAY_TABLA].Cliente(Cli_Id),
+	FOREIGN KEY (Vi_AutoPatente) 	REFERENCES [HAY_TABLA].Automovil(Auto_Patente),
+	FOREIGN KEY (Vi_IdTurno) 		REFERENCES [HAY_TABLA].Turno(Turno_Id)
+);
+
+CREATE TABLE [HAY_TABLA].Rendicion(
+	Rendicion_Nro 					NUMERIC(18,0) IDENTITY(1,1) NOT NULL,
+	Cho_Id 							INT NOT NULL,
+	Turno_Id 						INT NOT NULL,
+	Rendicion_Fecha 				DATETIME DEFAULT GETDATE() NOT NULL,
+	Rendicion_Total 				NUMERIC(18,2) NOT NULL,
+	PorcentajeDePago 				NUMERIC(4,2) NOT NULL,
+	CONSTRAINT PKRendicion			PRIMARY KEY (Rendicion_Nro),
+	FOREIGN KEY (Turno_Id) 			REFERENCES [HAY_TABLA].Turno(Turno_Id),
+	FOREIGN KEY (Cho_Id)   			REFERENCES [HAY_TABLA].Chofer(Cho_Id)
+);
+
+CREATE TABLE [HAY_TABLA].Factura(
+	Factura_Nro 					NUMERIC(18,0) IDENTITY(1,1) NOT NULL,
+	Factura_Fecha_Inicio 			DATETIME NOT NULL,
+	Factura_Fecha_Fin 				DATETIME NOT NULL,
+	Cli_Id 							INT NOT NULL,
+	Factura_Total 					NUMERIC(18,2) NOT NULL,
+	FOREIGN KEY (Cli_Id)   			REFERENCES [HAY_TABLA].Cliente(Cli_Id)
+);
+
+/* ############################## INSERTS A TABLAS MAESTRAS ############################## */
+
+/* INSERTS A USUARIOS */
 
 INSERT INTO [HAY_TABLA].[Usuarios] (Usu_Username,Usu_Password)
 VALUES ('admin', HASHBYTES('SHA2_256', 'w23e'))
 
-INSERT INTO [HAY_TABLA].[Usuarios]
-           ([Usu_Username]
-           ,[Usu_Password])
+INSERT INTO [HAY_TABLA].[Usuarios] (Usu_Username,Usu_Password)
 SELECT DISTINCT [Chofer_Dni],HASHBYTES('SHA2_256',CAST(Chofer_Dni AS VARCHAR(30) ))
 FROM [gd_esquema].[Maestra] 
 UNION
@@ -31,21 +162,8 @@ FROM [gd_esquema].[Maestra]
 WHERE Chofer_Dni IS NOT NULL AND Cliente_Dni IS NOT NULL
 ORDER BY 1
 
-/*CHOFER************************************************/
+/* INSERTS A CHOFERES */
 
-CREATE TABLE [HAY_TABLA].Chofer (
-    Cho_Id int NOT NULL IDENTITY(1,1),
-	Cho_Usuario VARCHAR(30), /*** FK **/
-    Cho_Nombre VARCHAR(30) NOT NULL,
-    Cho_Apellido varchar(255),
-	Cho_DNI numeric(18, 0),
-	Cho_Mail varchar(50),
-	Cho_Telefono numeric(18, 0),
-	Cho_Direccion varchar(255),
-	Cho_FechaNacimiento datetime,
-	CONSTRAINT PK_Chofer PRIMARY KEY (Cho_ID),
-	FOREIGN KEY (Cho_Usuario) REFERENCES [HAY_TABLA].Usuarios(Usu_Username)
-);
 INSERT INTO [HAY_TABLA].Chofer (Cho_Usuario,Cho_Nombre,Cho_Apellido,Cho_DNI,Cho_Mail,Cho_Telefono,Cho_Direccion,Cho_FechaNacimiento)
 SELECT DISTINCT
 	   CAST(Chofer_Dni AS VARCHAR(30))
@@ -65,22 +183,8 @@ ORDER BY
       ,Chofer_Mail
       ,Chofer_Fecha_Nac
 
-/*CLIENTE************************************************/
+/* INSERTS A CLIENTES */	  
 
-CREATE TABLE [HAY_TABLA].Cliente(
-	Cli_Id int NOT NULL IDENTITY(1,1),
-	Cli_Usuario VARCHAR(30), /** FK **/
-	Cli_Nombre varchar(255),
-	Cli_Apellido varchar(255),
-	Cli_DNI numeric(18, 0),
-	Cli_Mail varchar(255),
-	Cli_Telefono numeric(18, 0),
-	Cli_Direccion varchar(255),
-	Cli_CodigoPostal numeric(10, 0),
-	Cli_FechaNacimiento datetime
-	CONSTRAINT PK_Cliente PRIMARY KEY (Cli_Id)
-	,FOREIGN KEY (Cli_Usuario) REFERENCES [HAY_TABLA].Usuarios(Usu_Username)
-);
 INSERT INTO [HAY_TABLA].Cliente (Cli_Usuario,Cli_Nombre, Cli_Apellido, Cli_DNI, Cli_Telefono, Cli_Direccion, Cli_Mail, Cli_FechaNacimiento)
 SELECT DISTINCT 
 	   CAST(Cliente_Dni AS VARCHAR(30))
@@ -100,18 +204,7 @@ ORDER BY
 	  ,Cliente_Mail
 	  ,Cliente_Fecha_Nac
 
-/*AUTOMOVIL************************************************/
-
-CREATE TABLE [HAY_TABLA].Automovil(
-	Auto_Id int NOT NULL IDENTITY(1,1),
-	Auto_Patente varchar(10),
-	Auto_Marca varchar(20),
-	Auto_Modelo varchar(255),
-	Auto_Licencia varchar(26),
-	Auto_Rodado varchar(10),
-	Auto_Habilitado BIT DEFAULT '1',
-	CONSTRAINT PK_Automovil PRIMARY KEY (Auto_Id)
-);
+/* INSERTS A AUTOMOVILES */	
 
 INSERT INTO [HAY_TABLA].Automovil (Auto_Marca
       ,Auto_Modelo
@@ -130,18 +223,7 @@ SELECT Auto_Marca
       ,[Auto_Licencia]
       ,[Auto_Rodado]
 
-/*TURNO************************************************/
-
-CREATE TABLE [HAY_TABLA].[Turno](
-	Turno_Id int NOT NULL IDENTITY(1,1),
-	Turno_HoraInicio numeric(18, 0),
-	Turno_HoraFin numeric(18, 0),
-	Turno_Descripcion varchar(255),
-	Turno_ValorKM numeric(18, 2),
-	Turno_PrecioBase numeric(18, 2),
-	Turno_Habilitado BIT DEFAULT '1',
-	CONSTRAINT PK_Turno PRIMARY KEY (Turno_Id)
-)
+/* INSERTS A TURNOS */	
 
 INSERT INTO [HAY_TABLA].Turno (Turno_HoraInicio, Turno_HoraFin, Turno_Descripcion, Turno_ValorKM, Turno_PrecioBase)
 SELECT DISTINCT 
@@ -152,34 +234,15 @@ SELECT DISTINCT
 	  Turno_Precio_Base
 FROM gd_esquema.Maestra;
 
-/*ASIGNACION DE TURNOS************************************************/
-
-CREATE TABLE [HAY_TABLA].AsignacionDeTurnos(
-	Asignacion_Id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	Turno_Id int NOT NULL,
-	Cho_Id int NOT NULL,
-	Auto_Id int NOT NULL
- )
-
-CREATE TABLE [HAY_TABLA].[ROL] (
-	[Id_Rol]					INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	[Nombre]					VARCHAR(50) NOT NULL,
-	[Habilitado]				BIT DEFAULT 1
-)
-GO
+/* INSERTS A ROL */	
 
 INSERT INTO [HAY_TABLA].[ROL] (Nombre)
-	VALUES ('Administrador'), ('Cliente'), ('Chofer')
+VALUES ('Administrador'), ('Cliente'), ('Chofer')
 
-CREATE TABLE [HAY_TABLA].[USUARIO_POR_ROL] (
-	[Id_Rol]					INT NOT NULL REFERENCES [HAY_TABLA].[ROL](Id_Rol),
-	[Nombre_Usuario]			VARCHAR(30) NOT NULL REFERENCES [HAY_TABLA].[Usuarios](Usu_Username),
-	[Habilitado]				BIT DEFAULT 1
-)
-GO
+/* INSERTS A USUARIO POR ROL */	
 
 INSERT INTO [HAY_TABLA].[USUARIO_POR_ROL] (Id_Rol, Nombre_Usuario)
-	VALUES (1, 'admin'), (2, 'admin'), (3, 'admin')
+VALUES (1, 'admin'), (2, 'admin'), (3, 'admin')
 
 INSERT INTO [HAY_TABLA].[USUARIO_POR_ROL]
         ([Id_Rol]
@@ -191,24 +254,20 @@ INSERT INTO [HAY_TABLA].[USUARIO_POR_ROL]
         ,[Nombre_Usuario])
 SELECT  3,Ch.Cho_Usuario FROM [HAY_TABLA].[Chofer] AS Ch
 
-CREATE TABLE [HAY_TABLA].[FUNCIONALIDAD] (
-	[Id_Funcionalidad]			INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	[Descripcion]				VARCHAR(255) NOT NULL	
-)
-GO
+/* INSERTS A FUNCIONALIDAD */	
 
 INSERT INTO [HAY_TABLA].[FUNCIONALIDAD] (Descripcion)
-	VALUES ('ABM de Rol'), ('ABM de Cliente'), ('ABM de Automóvil'),
-		('ABM de Chofer'), ('Registro de Viajes'), ('Rendición de cuenta del chofer'), ('Facturación a Cliente'), ('Listado Estadístico'), ('Ver Viajes'), ('Ver Redenciones'), ('Ver Facturación') 
-
-CREATE TABLE [HAY_TABLA].[FUNCIONALIDAD_POR_ROL] (
-	[Id_Funcionalidad]			INT NOT NULL REFERENCES [HAY_TABLA].[FUNCIONALIDAD](Id_Funcionalidad),
-	[Id_Rol]					INT NOT NULL REFERENCES [HAY_TABLA].[ROL](Id_Rol)
-)
-GO
-
+VALUES  ('ABM de Rol'), ('ABM de Cliente'), ('ABM de Automóvil'),
+		('ABM de Chofer'), ('Registro de Viajes'), ('Rendición de cuenta del chofer'),
+		('Facturación a Cliente'), ('Listado Estadístico'), ('Ver Viajes'),
+		('Ver Redenciones'), ('Ver Facturación') 
+		
+/* INSERTS A FUNCIONALIDAD POR ROL */	
+		
 INSERT INTO [HAY_TABLA].[FUNCIONALIDAD_POR_ROL] (Id_Funcionalidad, Id_Rol)
-	VALUES (1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1),(9,2),(9,3),(10,3),(11,2)
+VALUES (1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1),(9,2),(9,3),(10,3),(11,2)
+
+/* INSERTS A ASIGNACION DE TURNOS */	
 
 INSERT INTO [HAY_TABLA].AsignacionDeTurnos(Turno_Id,Cho_Id, Auto_Id)
 SELECT Turno.Turno_Id turno_id, Chofer.[Cho_Id] chofer_id, Automovil.[Auto_Id] auto_id
@@ -218,24 +277,8 @@ INNER JOIN [HAY_TABLA].[Turno] turno ON turno.[Turno_Descripcion] = maestra.Turn
 INNER JOIN [HAY_TABLA].[Automovil] automovil ON automovil.[Auto_Patente] = maestra.Auto_Patente
 GROUP BY   maestra.Chofer_Dni,  maestra.Auto_Patente, maestra.Turno_Descripcion, turno.[Turno_Id], chofer.[Cho_Id], automovil.[Auto_Id]
 
-CREATE TABLE [HAY_TABLA].Viaje(
-	Id_Viaje int NOT NULL IDENTITY(1,1),
-	Vi_IdChofer int, /** FK **/
-	Vi_IdCliente int, /** FK **/
-	Vi_AutoPatente varchar(10), /** FK **/
-	Vi_IdTurno int, /** FK **/
-	Vi_CantKilometros numeric(18, 0),
-	Vi_Inicio datetime,
-	Vi_Fin datetime,
-	Rendicion_Nro numeric(18,0),
-	Factura_Nro numeric(18,0),
-	Vi_ImporteTotal numeric(18, 2)
-	CONSTRAINT PKViaje PRIMARY KEY (Id_Viaje),
-	FOREIGN KEY (Vi_IdChofer) REFERENCES [HAY_TABLA].Chofer(Cho_Id),
-	FOREIGN KEY (Vi_IdCliente) REFERENCES [HAY_TABLA].Cliente(Cli_Id),
-	/*FOREIGN KEY (Vi_AutoPatente) REFERENCES [HAY_TABLA].Automovil(Auto_Patente),*/
-	FOREIGN KEY (Vi_IdTurno) REFERENCES [HAY_TABLA].Turno(Turno_Id)
-);
+/* INSERTS A VIAJES */	
+
 INSERT INTO [HAY_TABLA].Viaje (Vi_IdChofer, Vi_IdCliente,Vi_AutoPatente, Vi_IdTurno,Vi_CantKilometros,Vi_Inicio,Vi_Fin,Rendicion_Nro,Factura_Nro,Vi_ImporteTotal)
 SELECT 
 	Chofer.Cho_Id,
@@ -264,18 +307,9 @@ GROUP BY
 	m1.Turno_Valor_Kilometro,
 	Turno.Turno_Id,
 	Rendicion_Nro
-
-
-
-CREATE TABLE [HAY_TABLA].Rendicion(
-Rendicion_Nro numeric(18,0) Identity(1,1) NOT NULL,
-Cho_Id int NOT NULL,
-Turno_Id int NOT NULL,
-Rendicion_Fecha datetime DEFAULT GETDATE() NOT NULL,
-Rendicion_Total numeric(18,2) NOT NULL,
-PorcentajeDePago numeric(4,2) NOT NULL
-)
-
+	
+/* INSERTS A RENDICION */	
+	
 SET IDENTITY_INSERT HAY_TABLA.Rendicion ON
 INSERT INTO HAY_TABLA.Rendicion(Rendicion_Nro, Cho_Id, Turno_Id, Rendicion_Fecha, Rendicion_Total,PorcentajeDePago)
 SELECT DISTINCT(Rendicion_Nro), Cho_Id, Turno_Id, Rendicion_Fecha,SUM(Rendicion_Importe),30 
@@ -287,13 +321,7 @@ GROUP BY Rendicion_Nro, Cho_Id, Turno_Id, Rendicion_Fecha
 order by 1
 SET IDENTITY_INSERT HAY_TABLA.Rendicion OFF
 
-CREATE TABLE [HAY_TABLA].Factura(
-Factura_Nro numeric(18,0) Identity(1,1) NOT NULL,
-Factura_Fecha_Inicio datetime NOT NULL,
-Factura_Fecha_Fin datetime NOT NULL,
-Cli_Id int NOT NULL,
-Factura_Total numeric(18,2) NOT NULL
-)
+/* INSERTS A FACTURA */	
 
 SET IDENTITY_INSERT [HAY_TABLA].Factura ON
 INSERT INTO [HAY_TABLA].Factura (Factura_Nro, Factura_Fecha_Inicio, Factura_Fecha_Fin, Cli_Id, Factura_Total)
@@ -308,9 +336,9 @@ WHERE Factura_Nro IS NOT NULL
 GROUP BY Factura_Nro, Factura_Fecha_Inicio, Factura_Fecha_Fin, c.Cli_Id, Cliente_Dni
 ORDER BY Factura_Nro
 SET IDENTITY_INSERT [HAY_TABLA].Factura OFF
-
 GO
 
+/* ############################## PROCEDURES ############################## */	
 
 CREATE PROCEDURE [HAY_TABLA].bajaLogica @table varchar(20), @id int AS
 BEGIN
@@ -377,6 +405,8 @@ BEGIN
 END
 GO
 
+/* ############################## TRIGGERS ############################## */	
+
 CREATE TRIGGER crearUsuarioAlCliente ON [HAY_TABLA].Cliente FOR INSERT AS
 BEGIN
 	DECLARE @username numeric(18,0)
@@ -420,4 +450,3 @@ BEGIN
 
 END
 GO
-COMMIT TRAN
